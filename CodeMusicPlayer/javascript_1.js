993a4c29c2923cd9fb33d5a95137c72f38c6ed03
Khoinguyen002 <93420917+Khoinguyen002@users.noreply.github.com>
2021-12-21 12:03:12 +0700

    Update javascript_1.js

--- CodeMusicPlayer/javascript_1.js
+++ CodeMusicPlayer/javascript_1.js
@@ -13,7 +13,7 @@ const prevSongBtn = $('.btn-prev')
 const randomSongBtn = $('.btn-random')
 const repeatSongBtn = $('.btn-repeat')
 var listSongIndexRandomed =[]
-var songsApi = 'https://my-json-server.typicode.com/Khoinguyen002/Production/songs'
+var songsApi = 'http://localhost:3000/songs'
 
 
 const app = {
